26a83980e8b8266f5248b60d65cf71cdf55c5a5d
amar-nath1 <amarat279955@gmail.com>
2026-01-04 10:59:36 +0530

    skills grouped

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,21 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  groups = [
+    {
+      title: 'Frontend',
+      skills: ['Angular', 'React', 'TypeScript', 'HTML & CSS', 'Bootstrap', 'Ionic', 'RxJS', 'NgRx']
+    },
+    {
+      title: 'Backend',
+      skills: ['Node.js', 'REST APIs', 'SQL', 'Express.js']
+    },
+    {
+      title: 'Other',
+      skills: ['Testing (Jasmine/Karma)', 'GitHub Actions']
+    }
+  ];
+
   constructor() {}
 
 }
